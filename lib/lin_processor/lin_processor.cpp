b6ffb75867b1ef60a6cb46c8b15f185804c7fb96
Peglah <peglah@gmail.com>
2018-11-20 19:35:23 +0100

    Update changes to the LIN library
    Disable debug pins.

--- lib/lin_processor/lin_processor.cpp
+++ lib/lin_processor/lin_processor.cpp
@@ -141,23 +141,23 @@ namespace lin_processor {
   // LIN interface.
   DEFINE_INPUT_PIN(rx_pin, D, 2);
   // TODO: Not use, as of Apr 2014.
-  DEFINE_OUTPUT_PIN(tx1_pin, C, 2, 1);
+//  DEFINE_OUTPUT_PIN(tx1_pin, C, 2, 1);
 
   // Debugging signals.
-  DEFINE_OUTPUT_PIN(break_pin, C, 0, 0);
-  DEFINE_OUTPUT_PIN(sample_pin, B, 4, 0);
-  DEFINE_OUTPUT_PIN(error_pin, B, 3, 0);
-  DEFINE_OUTPUT_PIN(isr_pin, C, 3, 0);
-  DEFINE_OUTPUT_PIN(gp_pin, D, 6, 0);
+//  DEFINE_OUTPUT_PIN(break_pin, C, 0, 0);
+//  DEFINE_OUTPUT_PIN(sample_pin, B, 4, 0);
+//  DEFINE_OUTPUT_PIN(error_pin, B, 3, 0);
+//  DEFINE_OUTPUT_PIN(isr_pin, C, 3, 0);
+//  DEFINE_OUTPUT_PIN(gp_pin, D, 6, 0);
 
   // Called one during initialization.
   static inline void setupPins() {
     rx_pin::setup();
-    break_pin::setup();
-    sample_pin::setup();
-    error_pin::setup();
-    isr_pin::setup();
-    gp_pin::setup();
+//    break_pin::setup();
+//    sample_pin::setup();
+//    error_pin::setup();
+//    isr_pin::setup();
+//    gp_pin::setup();
   }
 
   // ----- ISR RX Ring Buffers -----
@@ -279,10 +279,10 @@ namespace lin_processor {
 
   // Private. Called from ISR and from setup (beofe starting the ISR).
   static inline void setErrorFlags(uint8 flags) {
-    error_pin::setHigh();
+//    error_pin::setHigh();
     // Non atomic when called from setup() but should be fine since ISR is not running yet.
     error_flags |= flags;
-    error_pin::setLow();
+//    error_pin::setLow();
   }
 
   // Called from main. Public. Assumed interrupts are enabled.
@@ -453,11 +453,11 @@ namespace lin_processor {
     }
 
     // Detected a break. Wait for rx high and enter data reading.
-    break_pin::setHigh();
+//    break_pin::setHigh();
 
     // TODO: set actual max count
     waitForRxHigh(255);
-    break_pin::setLow();
+//    break_pin::setLow();
 
     // Go process the data
     StateReadData::enter();
@@ -485,9 +485,9 @@ namespace lin_processor {
 
   inline void StateReadData::handleIsr() {
     // Sample data bit ASAP to avoid jitter.
-    sample_pin::setHigh();
+//    sample_pin::setHigh();
     const uint8 is_rx_high = rx_pin::isHigh();
-    sample_pin::setLow();
+//    sample_pin::setLow();
 
     // Handle start bit.
     if (bits_read_in_byte_ == 0) {
@@ -594,7 +594,7 @@ namespace lin_processor {
   // Interrupt on Timer 2 A-match.
   ISR(TIMER2_COMPA_vect)
   {
-    isr_pin::setHigh();
+//    isr_pin::setHigh();
     // TODO: make this state a boolean instead of enum? (efficency).
     switch (state) {
     case states::DETECT_BREAK:
@@ -613,6 +613,6 @@ namespace lin_processor {
     // jitter.
     isr_marker++;
 
-    isr_pin::setLow();
+//    isr_pin::setLow();
   }
 }  // namespace lin_processor
